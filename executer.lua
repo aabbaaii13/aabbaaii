2d31fa6c06cf6cb089fa26b5de89e60d727c6115
aabbaaii <172551149+aabbaaii13@users.noreply.github.com>
2025-11-18 10:58:59 +0500

    Update executer.lua

--- executer.lua
+++ executer.lua
@@ -1,6 +1,6 @@
 local support = {
-    [3101667897] = "https://raw.githubusercontent.com/CaseohCASEOH/aabbaaii/refs/heads/main/LOS.lua",
-    [121864768012064] = "https://raw.githubusercontent.com/CaseohCASEOH/aabbaaii/refs/heads/main/fishit.lua"
+    [3101667897] = "https://raw.githubusercontent.com/aabbaaii13/aabbaaii/refs/heads/main/LOS.lua",
+    [121864768012064] = "https://raw.githubusercontent.com/aabbaaii13/aabbaaii/refs/heads/main/fishit.lua"
 }
 
 local place = game.PlaceId
@@ -9,5 +9,5 @@ local url = support[place]
 if url then
     loadstring(game:HttpGet(url))()
 else
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/CaseohCASEOH/aabbaaii/refs/heads/main/2023479888634011.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/aabbaaii13/aabbaaii/refs/heads/main/2023479888634011.lua"))()
 end
