f289480121c6a20403df6dc3bc42b06dddca2062
aabbaaii <172551149+aabbaaii13@users.noreply.github.com>
2025-12-09 23:21:31 +0500

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -1,6 +1,6 @@
 local whitelist = {
-    ["g52glw6gwwdof0xxciixyjzz6yll6fxr043fnfv1q08c92g441"] = true,
-    ["nyiczsmgy23is17aph6x117c9f93zctwvqeg3wjgxl6zgrmt5w"] = true,
+    [""] = true,
+    [""] = true,
 }
 
 if not isfile("hwid.key") then
